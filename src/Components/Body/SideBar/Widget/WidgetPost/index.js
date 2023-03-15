41343188328ad86c5ad8b78a8ae52aa51702b83a
Mosab Dwaikat <mosabdwaikat@gmail.com>
2023-03-15 18:24:40 +0200

    Removed unnecessary unequality check

--- src/Components/Body/SideBar/Widget/WidgetPost/index.js
+++ src/Components/Body/SideBar/Widget/WidgetPost/index.js
@@ -4,8 +4,8 @@ const index = ({ img, title, body }) => {
   return (
     <div className="WidgetPost-container">
       <img src={img} alt="" className="WidgetPost-image" />
-      {title !== undefined && <h2 className="WidgetPost-title">{title}</h2>}
-      {body !== undefined && <p className="WidgetPost-body">{body}</p>}
+      {title && <h2 className="WidgetPost-title">{title}</h2>}
+      {body && <p className="WidgetPost-body">{body}</p>}
     </div>
   );
 };
