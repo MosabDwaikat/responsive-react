0026a5cd9ef062787da567da89e0f59d0d5cbe26
Mosab Dwaikat <mosabdwaikat@gmail.com>
2023-03-15 18:08:22 +0200

    Adjusted declaration location

--- src/Components/Body/SideBar/index.js
+++ src/Components/Body/SideBar/index.js
@@ -1,27 +1,25 @@
 import React from "react";
 import Widget from "./Widget";
 import "./SideBar.css";
-const index = () => {
-  let para =
-    "Composed of both the material and spirit realms, Runeterra is all that\
+const para =
+  "Composed of both the material and spirit realms, Runeterra is all that\
     separates the celestial powers of creation from the abyssal threat of\
     all undoing. This is a magical world unlike any other--inhabited by\
     peoples both fierce and wondrous.";
+const posts = [
+  { img: "imgs/yone.jpg", title: "YONE: THE UNFORGOTTEN" },
+  { img: "imgs/Irelia.jpg", title: "IRELIA: THE BLADE DANCER" },
+  { img: "imgs/Lucian.jpg", title: "LUCIAN: THE PURIFIER" },
+];
 
+const index = () => {
   return (
     <aside class="sidebar ">
       <Widget
         title="More Info"
         posts={[{ img: "imgs/runeterra.jpg", body: para }]}
       />
-      <Widget
-        title="SEE ALSO"
-        posts={[
-          { img: "imgs/yone.jpg", title: "YONE: THE UNFORGOTTEN" },
-          { img: "imgs/Irelia.jpg", title: "IRELIA: THE BLADE DANCER" },
-          { img: "imgs/Lucian.jpg", title: "LUCIAN: THE PURIFIER" },
-        ]}
-      />
+      <Widget title="SEE ALSO" posts={posts} />
     </aside>
   );
 };
